680b0dffadae6f8cc6a5bb9b640319f0b74679c9
Andreas Bosteen <andr63c1@edu.ucl.dk>
2020-11-17 11:33:53 +0100

    Update WeatherController.cs

--- Surfapplikation/SurfapplikationAPI/Controllers/WeatherController.cs
+++ Surfapplikation/SurfapplikationAPI/Controllers/WeatherController.cs
@@ -26,15 +26,15 @@ namespace SurfapplikationAPI.Controllers
                 HttpRequestMessage request = new HttpRequestMessage();
                 request.RequestUri = new Uri("https://api.stormglass.io/v2/weather/point?lat=55.403756&lng=10.402370&params=waterTemperature,airTemperature,cloudCover");
                 request.Headers.Add("Authentication-Token", "ede82dc6-2014-11eb-8ea5-0242ac130002-ede82e8e-2014-11eb-8ea5-0242ac130002");
-                request.Content = new StringContent("{'lat' : 55.6,'lng' : 9.9,'params' : 'waveHeight'}", Encoding.UTF8, "application/json");
-                
-                HttpResponseMessage response = await client.SendAsync(request);
 
+                HttpResponseMessage response = await client.SendAsync(request);
+                string JSON = await response.Content.ReadAsStringAsync();
 
                 weatherdata = JsonConvert.DeserializeObject<Root>(JSON);
 
-
-
+            }
+            return Ok(weatherdata);
+        }
 
 
 
