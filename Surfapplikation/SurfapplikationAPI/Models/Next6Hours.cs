620e25a8dd8ef77f3e5b8a54a4fb9ecefc7e30e7
Martin Cevik <mart706g@edu.ucl.dk>
2020-11-17 08:39:41 +0100

    Api added

---
+++ Surfapplikation/SurfapplikationAPI/Models/Next6Hours.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Surfapplikation.Models
+{
+    public class Next6Hours
+    {
+        public Summary summary { get; set; }
+        public Details details { get; set; }
+    }
+}
