e64e8d401384103dc868abcda0ba1025204d48eb
Martin Cevik <mart706g@edu.ucl.dk>
2020-11-17 09:14:04 +0100

    get method not working

--- Surfapplikation/SurfapplikationAPI/Controllers/WeatherController.cs
+++ Surfapplikation/SurfapplikationAPI/Controllers/WeatherController.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Surfapplikation.Models;
 
 namespace SurfapplikationAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("weather")]
     [ApiController]
     public class WeatherController : ControllerBase
     {
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeatherData()
+        {
+            List<Root> weatherdata = new List<Root>();
+            using (var client = new HttpClient())
+            {
+              
+                HttpResponseMessage response = await client.GetAsync("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=10&lon=20");
+
+                string JSON = await response.Content.ReadAsStringAsync();
+
+                weatherdata = JsonConvert.DeserializeObject<List<Root>>(JSON);
+
+                return Ok(weatherdata);
+            }
+
+        }
     }
 }
